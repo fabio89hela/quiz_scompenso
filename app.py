373f82ae3e87597b696519a296bbcfa4c6ea509d
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-14 14:03:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from crewai import Crew, Process, Agent, Task
 from langchain_core.callbacks import BaseCallbackHandler
